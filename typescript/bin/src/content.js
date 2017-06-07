41623db2f13301ce2feca46d05d60708c862e120
Derek Honerlaw <derek@Dereks-MacBook-Pro.local>
2017-06-07 00:08:14 -0400

    boring stuff

--- typescript/bin/src/content.js
+++ typescript/bin/src/content.js
@@ -1,2 +1,34 @@
-console.log("awesome");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var $ = require("jquery");
+function getLookupData() {
+    var images = [];
+    var urls = [];
+    var gifs = [];
+    $("a.title").each(function (index, obj) {
+        var href = $(obj).attr("href");
+        // @todo this is a shitty hack until we have better detection
+        if (/^https?:/.test(href)) {
+            if (/\.(png|jpeg|jpg)$/.test(href)) {
+                images.push(href);
+            }
+            else if (/\.(gif|gifv)$/.test(href)) {
+                gifs.push(href);
+            }
+            else {
+                urls.push(href);
+            }
+        }
+    });
+    return {
+        images: images,
+        urls: urls,
+        gifs: gifs
+    };
+}
+// check if any has been submitted to reddit before (reguardless if image or not)
+// check image with reverse image lookup service (google or tineye?)
+// no idea on gifs / videos
+// potentially - record all urls on reddit, potentially record file hashes to start own reverse image service
+console.log(getLookupData());
 //# sourceMappingURL=content.js.map
--- typescript/src/content.ts
+++ typescript/src/content.ts
@@ -1,1 +1,35 @@
-console.log("awesome");
+import * as $ from "jquery";
+
+
+function getLookupData(): object {
+    const images: any[] = [];
+    const urls: any[] = [];
+    const gifs: any[] = [];
+    $("a.title").each((index: number, obj: any) => {
+        const href: string = $(obj).attr("href");
+
+        // @todo this is a shitty hack until we have better detection
+        if (/^https?:/.test(href)) {
+            if (/\.(png|jpeg|jpg)$/.test(href)) {
+                images.push(href);
+            } else if (/\.(gif|gifv)$/.test(href)) {
+                gifs.push(href);
+            } else {
+                urls.push(href);
+            }
+        }
+    });
+    return {
+        images,
+        urls,
+        gifs
+    };
+}
+
+// check if any has been submitted to reddit before (reguardless if image or not)
+// check image with reverse image lookup service (google or tineye?)
+// no idea on gifs / videos
+
+// potentially - record all urls on reddit, potentially record file hashes to start own reverse image service
+
+console.log(getLookupData());
